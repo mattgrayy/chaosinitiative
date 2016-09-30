441a11e8ed098a117094ffdd204dcb09c22bf578
Dreggrim <Merlin123SS>
2016-09-30 18:11:43 +0100

    Added: bullet trails bullet bounce sound added particle effect when collision main menu Scene

---
+++ Sticky/Assets/Prefabs/Bullets with trails and sound (sprites included)/Bullets bounce/scripts/Shoot.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour
+{
+
+    public Rigidbody2D bulletbody;
+
+    // Use this for initialization
+    void Start()
+    {
+        bulletbody = this.GetComponent<Rigidbody2D>();
+
+        bulletbody.AddForce(new Vector2(0, -500));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+
+    }
+
+}
---
+++ Sticky/Assets/Prefabs/Bullets with trails and sound (sprites included)/Bullets bounce/scripts/Spark.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spark : MonoBehaviour {
+
+    public Transform sparkle;
+	public AudioClip bounce;
+
+
+	// Use this for initialization
+	void Start () {
+		sparkle.GetComponent<ParticleSystem> ().Stop();
+		GetComponent<AudioSource> ().clip = bounce;	
+	}
+	
+	void OnTriggerEnter2D()
+    {
+
+		sparkle.GetComponent<ParticleSystem> ().Play();
+        StartCoroutine(stopSparkles());
+		GetComponent<AudioSource> ().Play ();
+    }
+
+    IEnumerator stopSparkles()
+    {
+        yield return new WaitForSeconds(.1f);
+		sparkle.GetComponent<ParticleSystem> ().Stop();
+
+    }
+
+
+}
