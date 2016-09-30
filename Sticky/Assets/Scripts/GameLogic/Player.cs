e48fc76dc9fdc03262550b867ce1795fe86c5883
mattgrayy <Matt2.Gray@live.uwe.ac.uk>
2016-09-30 10:41:29 +0100

    Testing player movement

---
+++ Sticky/Assets/Scripts/GameLogic/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Actor {
+
+    public int playerNumber = 0;
+
+    void Update()
+    {
+        switch (playerNumber)
+        {
+            case 1:
+                transform.Translate(Vector2.right * Input.GetAxis("Mouse X") * 10 * Time.deltaTime); // Replace with "XAxis1"
+                break;
+            case 2:
+                transform.Translate(Vector2.right * Input.GetAxis("Mouse Y") * 10 * Time.deltaTime); // Replace with "XAxis2"
+                break;
+            case 3:
+                transform.Translate(Vector2.right * Input.GetAxis("Mouse ScrollWheel") * 100 * Time.deltaTime); // Replace with "XAxis3"
+                break;
+            default:
+                break;
+        }
+    }
+}
