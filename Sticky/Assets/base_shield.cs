7ea475c348a59ee39da21fe09034244f528eae9a
OwenRuddle <Owen2.Ruddle@live.uwe.ac.uk>
2016-09-30 17:30:11 +0100

    world shield
    world shield is in and works.

---
+++ Sticky/Assets/base_shield.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class base_shield : MonoBehaviour {
+    [SerializeField]  protected int ricocet = 1;
+    private bool TIMETODIE = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+    public void DestroySheild()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void LateUpdate()
+    {
+        if (ricocet < 1)
+        {
+           
+        }
+    }
+
+    IEnumerator kill()
+    {
+        
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForEndOfFrame();
+        
+        DestroySheild();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        TIMETODIE = true;
+
+        StartCoroutine("kill");
+
+        if (ricocet>0)
+        {
+            ricocet--;
+        }
+      
+    }
+
+
+}
+
+
