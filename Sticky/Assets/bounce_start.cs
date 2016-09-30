2f7d86f49345bfe14ff4d122c61d6c89e22ab914
OwenRuddle <Owen2.Ruddle@live.uwe.ac.uk>
2016-09-30 13:39:16 +0100

    minor change to bullet script
    as title says

---
+++ Sticky/Assets/bounce_start.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bounce_start : MonoBehaviour {
+
+    // Use this for initialization
+    public float x;
+    public float y;
+    public Rigidbody2D ridg;
+    public GameObject ball;
+    public int bounces;
+  
+	void Start () {
+        ridg = GetComponent<Rigidbody2D>();
+        ridg.AddForce(new Vector2(x,y));
+    }
+
+    void OnCollision2D()
+    {
+    }
+
+
+    
+    void OnCollisionEnter2D(Collision2D col)
+    {
+       
+        if (col.gameObject.tag == "wall3")
+        {
+         
+            if (bounces > 0)
+            {
+                bounces--;
+            }
+            else if (bounces == 0)
+            {
+                Destroy(ball);
+            }
+        }
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+       
+	}
+}
