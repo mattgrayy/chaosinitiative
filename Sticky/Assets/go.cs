f6e4d4f9fa07c709a1cd1e403c36088bbe521409
Dreggrim <Merlin123SS>
2016-10-03 13:36:54 +0100

    added in a death explosion for the enemies

---
+++ Sticky/Assets/go.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class go : MonoBehaviour {
+
+    public ParticleSystem ex1, ex2;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            ex1.Play();
+            ex2.Play();
+
+
+        }
+
+
+
+    }
+}
