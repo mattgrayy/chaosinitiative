87b31df022d9d86887852cd0b424b6305b92e203
Shaun Landy <Shaun2.Landy@live.uwe.ac.uk>
2016-10-04 15:01:09 +0100

    Background Preparation

---
+++ Sticky/Assets/Scripts/GameLogic/Background.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour
+{
+    [SerializeField] private ScrollingBackground backgrounds = null;
+
+    private void Update()
+    {
+
+    }
+}
---
+++ Sticky/Assets/Scripts/GameLogic/ScrollingBackground.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class ScrollingBackground
+{
+    public Sprite sprite = null;
+    public float movementSpeed = 1.0f;
+}
