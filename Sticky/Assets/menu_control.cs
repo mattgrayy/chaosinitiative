ec6d49fb602d93686b5c902c3b13fab3fe4c5891
OwenRuddle <Owen2.Ruddle@live.uwe.ac.uk>
2016-10-04 14:44:42 +0100

    added game over screen

---
+++ Sticky/Assets/menu_control.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menu_control : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	if(Input.GetButtonDown("0Button1"))
+        {
+            Application.LoadLevel(0);
+        }
+        if (Input.GetButtonDown("0Button3"))
+        {
+            Application.Quit();
+        }
+        if (Input.GetButtonDown(" 1Button1"))
+        {
+            Application.LoadLevel(0);
+        }
+        if (Input.GetButtonDown("1Button3"))
+        {
+            Application.Quit();
+        }
+        if (Input.GetButtonDown( "2Button1"))
+        {
+            Application.LoadLevel(0);
+        }
+        if (Input.GetButtonDown("2Button3"))
+        {
+            Application.Quit();
+        }
+    }
+}
